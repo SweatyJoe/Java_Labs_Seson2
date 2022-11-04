a4142661b4d2b90700ffc39da18345692dac9ab9
Andrey <55462720+SweatyJoe@users.noreply.github.com>
2022-11-04 22:01:18 +0300

    lab8_V14.0.3
    get branch, rate, but no range

--- lab8/src/DOMExample.java
+++ lab8/src/DOMExample.java
@@ -9,7 +9,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class DOMExample {
@@ -20,29 +19,76 @@ public class DOMExample {
         DocumentBuilder builder = factory.newDocumentBuilder();
 
         Document doc = builder.parse(new File("src\\file.xml"));
-        NodeList bankElements = doc.getDocumentElement().getElementsByTagName("branch");
+        NodeList bankElements = doc.getDocumentElement().getElementsByTagName("branch"); //берём все branch
         try {
-            for (int i = 0; i < bankElements.getLength(); i++) {
+            for (int i = 0; i < bankElements.getLength(); i++) {  //перебираем
                 Node bankBranch = bankElements.item(i);
-                NamedNodeMap attributesBranch = bankBranch.getAttributes();
-                System.out.println("\nName:" + attributesBranch.getNamedItem("name").getNodeValue());
-                NodeList bankElementsNew = doc.getDocumentElement().getElementsByTagName("rate");
-
+                //NamedNodeMap attributesBranch = bankBranch.getAttributes();
+                System.out.println("\nName:" + bankBranch.getAttributes().getNamedItem("name").getNodeValue());
+                NodeList bankElementsNew = bankBranch.getChildNodes();
                 for (int j = 0; j < bankElementsNew.getLength(); j++) {
+                    Node nodeRate = bankElementsNew.item(j);
+                    try {
+                        System.out.println("Currency: " + nodeRate.getAttributes().getNamedItem("currency").getNodeValue() +
+                                " Units: " + nodeRate.getAttributes().getNamedItem("Units").getNodeValue());
+
+                        NodeList nodeRange = nodeRate.getChildNodes();
+                        try {
+                            for (int k = 0; k < nodeRange.getLength(); k++) {
+                                Node node = nodeRange.item(k);
+                                System.out.println("\tmin: " + node.getAttributes().getNamedItem("min-amount").getNodeValue() +
+                                        " max: " + node.getAttributes().getNamedItem("max-amount").getNodeValue());
+                            }
+
+                        } catch (NullPointerException e) {
+                            continue;
+                        }
+                    } catch (NullPointerException e) {
+                        continue;
+                    }
+
+
+                }
+
+                //Node bb = bankElementsNew;
+                //System.out.println(bb.getLocalName().toString());
+                /*if (bb != null) {
+                    //NodeList bankElementsRange = doc.getDocumentElement().getElementsByTagName("range");
+                    NodeList bankElementsRange = bb.item(i).getChildNodes();
+                    if (bankElementsRange != null) {
+                        for (int j = 0; j < bankElementsRange.getLength(); j++) {
+                            Node bRange = bankElementsRange.item(j);
+                            NamedNodeMap mapRange = bRange.getAttributes();
+                            NodeList nodeList = bRange.getChildNodes();
+                            System.out.println("\tmin\\max[" + nodeList.item(0).getTextContent() + nodeList.item(1).getTextContent() + "]\n" +
+                                    "\tMin\\max: [" + mapRange.getNamedItem("min-amount").getNodeValue() + "][" + mapRange.getNamedItem("max-amount").getNodeValue() + "]");
+                        }
+                    }
+                }*/
+
+               /* for (int j = 0; j < bankElementsNew.getLength(); j++) {
                     Node bankRate = bankElementsNew.item(j);
                     NamedNodeMap attributesRate = bankRate.getAttributes();
-                    System.out.print("\tCur: " + attributesRate.getNamedItem("currency").getNodeValue() + "Un: " + attributesRate.getNamedItem("Units").getNodeValue());
-                    NodeList bankElementsRange = doc.getDocumentElement().getElementsByTagName("range");
+                    try {
+                        if (attributesRate.getNamedItem("currency").getTextContent().length() < 4) {
+                            System.out.print("\tCur: " + attributesRate.getNamedItem("currency").getNodeValue() + "Un: " + attributesRate.getNamedItem("Units").getNodeValue());
+                            NodeList bankElementsRange = doc.getDocumentElement().getElementsByTagName("range");
 
-                    for (int k = 0; k < bankElementsRange.getLength(); k++) {
-                        Node bankRange = bankElementsRange.item(k);
-                        NamedNodeMap attributesRange = bankRange.getAttributes();
-                        System.out.println("\tMin\\max: [" + attributesRange.getNamedItem("min-amount").getNodeValue() + "][" + attributesRange.getNamedItem("max-amount").getNodeValue() + "]");
-                        NodeList buy = doc.getDocumentElement().getElementsByTagName("buy");
-                        NodeList sell = doc.getDocumentElement().getElementsByTagName("sell");
-                        System.out.println("\tbuy\\sell :[" + buy.item(k).getTextContent() + "][" + sell.item(k).getTextContent() + "]");
+                            for (int k = 0; k < bankElementsRange.getLength(); k++) {
+                                Node bankRange = bankElementsRange.item(k);
+                                NamedNodeMap attributesRange = bankRange.getAttributes();
+                                System.out.println("\tMin\\max: [" + attributesRange.getNamedItem("min-amount").getNodeValue() + "][" + attributesRange.getNamedItem("max-amount").getNodeValue() + "]");
+                                NodeList buy = doc.getDocumentElement().getElementsByTagName("buy");
+                                NodeList sell = doc.getDocumentElement().getElementsByTagName("sell");
+                                System.out.println("\t\tbuy\\sell :[" + buy.item(k).getTextContent() + "][" + sell.item(k).getTextContent() + "]");
+                            }
+                        } else {
+                            System.out.println(attributesRate.getNamedItem("currency").getTextContent());
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                }
+                }*/
                 /*banks.add(new Bank(attributes.getNamedItem("name").getNodeValue(),
                         new Currency(attributes.getNamedItem("currency").getNodeValue(),
                                 Integer.parseInt(attributes.getNamedItem("Units").getNodeValue()),
